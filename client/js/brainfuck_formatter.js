bfb7ca741cb05fe740bc186649c90bf3b7326e40
Jorel Ali (Skepter) <skepter.misc@gmail.com>
2017-09-13 18:31:40 +0100

    update to format system, added > and < operators

--- client/js/brainfuck_formatter.js
+++ client/js/brainfuck_formatter.js
@@ -18,8 +18,38 @@ function formatString(inputStr) {
 			next = code.charAt(charPointer + 1);
 		} catch(err) { }
 
+		var c = code.charAt(charPointer);
+
+		switch(c) {
+			case ">":
+			case "<":
+				switch(prev) {
+					case ">":
+					case "<":
+						output = output.concat(c);
+						break;
+					case "#":
+						output = output.concat(indent(indentationLevel)).concat(c);
+						break;
+					default:
+						output = output.concat("\n").concat(indent(indentationLevel)).concat(c);
+						break;
+				}
+				break;
+			case "+":
+			case "-":
+
+		}
+
 		//format goes here :D
 	}
 
 	//set text output
+}
+
+function indent(indentationLevel) {
+	var str = "";
+	for (var i = 0; i < indentationLevel; i++)
+		str = str + "\t";
+	return str;
 }
